ec0932e344b5143e206fdd50698e7f7aa54e2913
Ivan <ivan.ornes@gmail.com>
2013-12-13 17:21:38 +0100

    initial

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('odiometroApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
